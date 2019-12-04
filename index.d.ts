77d67fa1ff8f0d0c4f4a64f8de62dd9bfc4111e5
Marco Scabbiolo <marco.scabbiolo@gointegro.com>
2019-12-04 15:51:37 -0300

    typescript types

---
+++ index.d.ts
@@ -0,0 +1,15 @@
+export interface RNAccountManagerAccount {
+  _index: number
+  name: string
+  type: string
+}
+
+export interface RNAccountManager {
+  getAccountsByType(accountType: string): Promise<RNAccountManagerAccount[]>
+  addAccountExplicitly(accountType: string, userName: string, password: string): Promise<RNAccountManagerAccount>
+  removeAccount(accountObject: RNAccountManagerAccount): Promise<{}>
+  setUserData(accountObject: RNAccountManagerAccount, key: string, data: string): Promise<{}>
+  getUserData(accountObject: RNAccountManagerAccount, key: string): Promise<{ value: string }>
+}
+
+export default RNAccountManager
